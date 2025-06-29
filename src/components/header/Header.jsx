3a98819ae8fdd416aa584e0b1f2112c153fdef85
Vladimir <vovandrop@gmail.com>
2025-06-29 15:46:55 +0300

    Правки по ревью

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,30 +1,32 @@
+import { useState } from 'react';
 import { Button } from '../button/button';
 import { H1 } from '../h1/H1';
 import Toggle from '../toggle/Toggle';
 import styles from './Header.module.css';
 
 export const Header = () => {
+  const [isOn, setIsOn] = useState(false);
+
+  const handleToggle = () => {
+    setIsOn(!isOn);
+  };
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <div className={styles.avatar}>{/* сюда можно ссылку */}</div>
+        <img className={styles.avatar} />
         <H1 title={'Название команды'} />
       </div>
       <nav className={styles.nav}>
-        <Button
-          title={'Услуги'}
-          onClick={() => {
-            // можно navigate, а можно и ссылки...
-          }}
-        />
-        <Button
-          title={'Избранные'}
-          onClick={() => {
-            // можно navigate, а можно и ссылки...
-          }}
-        />
-        <Toggle />
-        <div className={styles.button}>Тёмная тема</div>
+        <th>
+          <a href="...">Услуги</a>
+        </th>
+        <th className={styles.th}>
+          <a href="/favorites">Избранные</a>
+        </th>
+        <th className={styles.toggle}>
+          <Toggle isOn={isOn} handleToggle={handleToggle} />
+          <div>Тёмная тема</div>
+        </th>
       </nav>
     </header>
   );
